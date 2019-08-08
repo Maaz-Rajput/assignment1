b343c46359574bedf2f9cd7368b3486fee968859
Your Name <maazrajput38@gmail.com>
2019-08-08 12:07:50 -0700

    java code

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,15 @@
+
+package helloworld;
+
+
+public class Helloworld {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("This is my first code");
+        
+        
+    }
+    
+}
---
+++ src/helloworld/arthimaticoperator.java
@@ -0,0 +1,29 @@
+
+package helloworld;
+
+
+
+    public class arthimaticoperator {
+        public static void main(String[] args) {
+    
+        int a = 12;
+        int b = 4;
+        
+        
+        int c = a+b;
+        System.out.println("Addition of two numbers" + c);
+        
+        int d = a - b;
+        System.out.println("Subtraction of two numbers" + d);
+        
+        int e = a * b;
+        System.out.println("Multilication of two numbers" + e);
+        
+        int f = a / b;
+        System.out.println("Adition of two numbers" + f);
+        
+        int g = a % b;
+        System.out.println("Modules of two numbers" + g);
+        
+        }
+    }
